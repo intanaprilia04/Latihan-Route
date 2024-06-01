e796bac68223a5466201615c91f951db7e4963d4
intanaprilia04 <apriliaintan062@gamil.com>
2024-06-02 01:26:41 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Selamat Datang di Halaman Blog Saya</h1>
+    <p> Ini adalah blog saya</p>
+
+    <a href="{{route('posts.index')}}"><button>View all Posts</button>
+</body>
+</html>
